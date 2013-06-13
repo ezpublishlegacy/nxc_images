14e941fb6afd2c0388bad31519fb7ab36bca477f
Serhey Dolgushev <sd@nxc.no>
2013-06-13 18:37:46 +0300

    fixing simple object relations

---
+++ settings/override.ini.append.php
@@ -0,0 +1,9 @@
+<?php /* #?ini charset="utf-8"?
+
+[edit_related_image]
+Source=content/datatype/edit/ezobjectrelation.tpl
+MatchFile=edit_related_image.tpl
+Subdir=templates
+Match[attribute_identifier]=image
+
+*/ ?>
